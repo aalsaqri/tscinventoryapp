6cb5411b18890f2d4befdeecfc8e0d2ebd589446
Abdallah Alsaqri <aalsaqri@gmail.com>
2024-10-14 01:00:24 -0400

    Updating database config to work with postgres

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///stock.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace(
+        'postgres://', 'postgresql://', 1  # Ensure compatibility with SQLAlchemy
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB upload limit
